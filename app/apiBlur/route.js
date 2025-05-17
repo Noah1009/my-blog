a0f7ad3550d969cc88ddcd60d616e28d5ddce426
Urasaki <urasaki@UrasakinoMacBook-Pro.local>
2025-05-17 22:43:19 +0900

    Fix imports and install plaiceholder

--- app/apiBlur/route.js
+++ app/apiBlur/route.js
@@ -1,6 +1,6 @@
 // app/apiBlur/route.js
 // /app/api/blur/route.js
-import { getPlaiceholder } from "@plaiceholder/next";
+import { getPlaiceholder } from "plaiceholder";
 import fetch from "node-fetch";
 
 export async function POST(request) {
